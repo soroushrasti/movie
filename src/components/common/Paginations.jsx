c6c9dd27280c16825767ed2862d50175576ea89c
Soroush Rasti <rasti.soroush@gmail.com>
2020-11-14 20:35:02 +0330

    pagination-2

--- src/components/common/Paginations.jsx
+++ src/components/common/Paginations.jsx
@@ -6,14 +6,14 @@ const Pagination = (props) => {
     const pageCount=Math.ceil(itemsCount/pageSize);
     const pages=_.range(1,pageCount+1)
 
-    return <nav>
+    return (<nav>
         <ul className="pagination">
-            {pages.map(page=> 
+            {pages.map(page=> (
             <li key={page} className="page-item"><a href="ii" 
-            onClick={onPageChange(page)} className="page link"></a>{page}</li>
-                )} 
-        </ul>
-    </nav> ;
+            onClick={onPageChange(page)} className="page-link">{page}</a></li>
+                ))} 
+         </ul>
+    </nav> );
 }
  
 export default Pagination;
